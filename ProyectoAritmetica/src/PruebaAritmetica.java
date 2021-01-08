184019caa765b90d2058b12cf2b350c8fdd84760
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-01-08 17:13:56 +0000

    Alcance de variables, paso por referencia

--- ProyectoAritmetica/src/PruebaAritmetica.java
+++ ProyectoAritmetica/src/PruebaAritmetica.java
@@ -1,10 +1,31 @@
+import javax.naming.PartialResultException;
+
 public class PruebaAritmetica {
     public static void main(String[] args) {
-        //creamos un objeto de la clase Aritmetica
-        Aritmetica aritmetica = new Aritmetica();
 
-        //Llamamso al metodo sumar y recibimos el valor devuelto
-        int resultado = aritmetica.sumar(5,3);
-        System.out.println("resultado = " + resultado );
+        //variables locales
+        int operandoA = 6;
+        int operandoB = 2;
+
+        //creamos un objeto de la clase Aritmetica enviado argumentos
+        Aritmetica objeto1 = new Aritmetica(operandoA, operandoB);
+
+        //imprimimos los valores de los objetos
+        System.out.println("objetoA = " + operandoA);
+        System.out.println("objeto B = " + operandoB);
+
+        // imprimimos el resultado suma
+        System.out.println(" \nResultado suma: " + objeto1.sumar());
+
+        //imprimimos el resultado de la resta
+        System.out.println("\nResultado resta = "+objeto1.restar());
+
+        // imprimimos el resultado de la division
+        System.out.println("\nResultado division = " +objeto1.dividir());
+
+        //imprimimos el resultado de la multiplicacion
+        System.out.println("\nResultado multiplicacion = " + objeto1.multiplicar());
     }
+
+
 }
